3f00f598c486c19b76262368c87b7e97f68368e7
Leon Fedden <leon.fedden@astrazeneca.com>
2023-05-31 07:56:00 +0000

    getting private gpt working nicely

--- question-answering/constants.py
+++ question-answering/constants.py
@@ -2,22 +2,24 @@ from chromadb.config import Settings
 
 # The folder you want your vectorstore in
 PERSIST_DIRECTORY: str = "db"
-# supports LlamaCpp or GPT4All
-MODEL_TYPE: str = "GPT4All"
+# supports LlamaCpp, GPT4All, HuggingFaceHub, HuggingFacePipeline
+MODEL_TYPE: str = "HuggingFacePipeline" # "GPT4All"
 # Path to your GPT4All or LlamaCpp supported LLM
-MODEL_PATH: str = "ggml-gpt4all-j-v1.3-groovy.bin"
+MODEL_PATH: str = "models/ggml-gpt4all-j-v1.3-groovy.bin"
+# Token.
+HUGGINGFACE_HUB_API_TOKEN: str = "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"
 # SentenceTransformers embeddings model name (see https://www.sbert.net/docs/pretrained_models.html)
 EMBEDDINGS_MODEL_NAME: str = "all-MiniLM-L6-v2"
 # Maximum token limit for the LLM model
 MODEL_N_CTX: int = 1000
 # The amount of chunks (sources) that will be used to answer a question
-TARGET_SOURCE_CHUNKS: int = 4
+TARGET_SOURCE_CHUNKS: int = 3
 # Where the documents are.
 SOURCE_DIRECTORY: str = "documents"
 # Debugble for loops..
 SINGLE_PROCESS: bool = True
 # Chunks to be embedded.
-CHUNK_SIZE: int = 500
+CHUNK_SIZE: int = 300
 CHUNK_OVERLAP: int = 50
 # Define the Chroma settings
 CHROMA_SETTINGS = Settings(
@@ -25,3 +27,4 @@ CHROMA_SETTINGS = Settings(
     persist_directory=PERSIST_DIRECTORY,
     anonymized_telemetry=False,
 )
+USE_PROMPT: bool = True
--- question-answering/privateGPT.py
+++ question-answering/privateGPT.py
@@ -1,18 +1,65 @@
 #!/usr/bin/env python3
-import argparse
 
-from langchain.chains import RetrievalQA
+import torch
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts import PromptTemplate
 from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import HuggingFacePipeline
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+    pipeline,
+)
+from rich.columns import Columns
+from rich.console import Console
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.prompt import Prompt
 
 import constants
 
 
+def load_model(model_id: str):
+    """
+    Select a model on huggingface.
+    If you are running this for the first time, it will download a model for you.
+    subsequent runs will use the model from the disk.
+    """
+    n_gpus = torch.cuda.device_count()
+    max_memory = {i: "10GB" for i in range(n_gpus)}
+    model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=model_id,
+        cache_dir="/shared/llm/huggingface",
+        load_in_4bit=True,
+        device_map="sequential",
+        max_memory=max_memory,
+        torch_dtype=torch.bfloat16,
+        quantization_config=BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+        ),
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=8192,
+        temperature=0,
+        top_p=0.95,
+        repetition_penalty=1.15,
+    )
+
+    local_llm = HuggingFacePipeline(pipeline=pipe)
+
+    return local_llm
+
+
 def main():
-    # Parse the command line arguments
-    args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=constants.EMBEDDINGS_MODEL_NAME)
     db = Chroma(
         persist_directory=constants.PERSIST_DIRECTORY,
@@ -20,79 +67,53 @@ def main():
         client_settings=constants.CHROMA_SETTINGS,
     )
     retriever = db.as_retriever(search_kwargs={"k": constants.TARGET_SOURCE_CHUNKS})
-    # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    # Prepare the LLM
-    match constants.MODEL_TYPE:
-        case "LlamaCpp":
-            llm = LlamaCpp(
-                model_path=constants.MODEL_PATH,
-                n_ctx=constants.MODEL_N_CTX,
-                callbacks=callbacks,
-                verbose=False,
+    llm = load_model(model_id="timdettmers/guanaco-33b-merged")
+    if constants.USE_PROMPT:
+        with open("prompt.txt", "r") as stream:
+            prompt_template: str = stream.read()
+            prompt = PromptTemplate(
+                template=prompt_template, input_variables=["summaries", "question"]
             )
-        case "GPT4All":
-            llm = GPT4All(
-                model=constants.MODEL_PATH,
-                n_ctx=constants.MODEL_N_CTX,
-                backend="gptj",
-                callbacks=callbacks,
-                verbose=False,
-            )
-        case _default:
-            print(f"Model {constants.MODEL_TYPE} not supported!")
-            exit
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
+            chain_type_kwargs = {"prompt": prompt}
+    else:
+        chain_type_kwargs = None
+    qa = RetrievalQAWithSourcesChain.from_chain_type(
         chain_type="stuff",
+        chain_type_kwargs=chain_type_kwargs,
+        llm=llm,
         retriever=retriever,
-        return_source_documents=not args.hide_source,
+        return_source_documents=True,
     )
     # Interactive questions and answers
+    console = Console()
     while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
-
+        console.clear()
+        new_lines: str = "\n" * ((console.height // 2) - 1)
+        question: str = Prompt.ask(f"{new_lines}  Ask anything about the DL team")
         # Get the answer from the chain
-        res = qa(query)
-        answer, docs = (
-            res["result"],
-            [] if args.hide_source else res["source_documents"],
-        )
-
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        print(answer)
-
+        res = qa({"question": question})
+        answer, docs = res["answer"], res["source_documents"]
+        qa_panel = Panel(f"[b]Question:[/b] {question}\n[yellow]{answer.rstrip()}")
         # Print the relevant sources used for the answer
+        document_panels = []
         for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description=(
-            "privateGPT: Ask questions to your documents without an internet "
-            "connection, using the power of LLMs."
+            document_panels.append(
+                Panel(
+                    document.page_content,
+                    title=(
+                        f"[grey89][u][i]Source Document: "
+                        f"{document.metadata['source']}[/i][/grey89][/u]"
+                    ),
+                )
+            )
+        layout = Layout(size=10)
+        layout.split_row(
+            qa_panel,
+            Columns(document_panels),
         )
-    )
-    parser.add_argument(
-        "--hide-source",
-        "-S",
-        action="store_true",
-        help="Use this flag to disable printing of source documents used for answers.",
-    )
-    parser.add_argument(
-        "--mute-stream",
-        "-M",
-        action="store_true",
-        help="Use this flag to disable the streaming StdOut callback for LLMs.",
-    )
-    return parser.parse_args()
+        console.clear()
+        console.print(layout)
+        input("")
 
 
 if __name__ == "__main__":
